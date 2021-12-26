d005adccf464f9a1e98f161105d00f3397a4c655
VladimirBrejcha <vladimirbrejcha@icloud.com>
2021-12-26 13:25:52 +0300

    Store state between threads

--- source/LaunchkeyMiniMk3Controller.rb
+++ source/LaunchkeyMiniMk3Controller.rb
@@ -1,3 +1,24 @@
+## State
+
+module Color
+  DISABLED = 0
+  BLUE = 40
+  VIOLET = 49
+  PINK = 53
+  YELLOW = 13
+  GREEN = 21
+  RED = 5
+end
+
+module PadState
+  DISABLED = Color::DISABLED
+  ENABLED = Color::PINK
+end
+
+configured = get[:configured]
+
+pads = (96..103).to_a + (112..119).to_a
+
 ## Events
 
 live_loop :inControl_events do
@@ -17,28 +38,9 @@ end
 live_loop :pad_events do
   use_real_time
   note, velocity = sync "/midi:launchkey_mini_mk3_daw_port:1/note_on"
-  if velocity != 0
+  if velocity != 0 && pads.include?(note)
     switch_pad_light note
   end
-end
-
-## State
-
-pads = (96..103).to_a + (112..119).to_a
-
-state = pads.reduce({}) do |hash, value|
-  hash[value] = Color::DISABLED
-  hash
-end
-
-module Color
-  DISABLED = 0
-  BLUE = 40
-  VIOLET = 49
-  PINK = 53
-  YELLOW = 13
-  GREEN = 21
-  RED = 5
 end
 
 ## Actions
@@ -55,18 +57,63 @@ end
 # Changes color of a pad
 define :light_pad do |pad, color|
   midi_raw 144, pad, color
+  state = get[:pads_state].to_h
   state[pad] = color
+  set :pads_state, state
 end
 
-# Switch enabled/disabled
 define :switch_pad_light do |pad|
-  if state[pad] == Color::DISABLED
-    light_pad pad, Color::PINK
+  if get[:pads_state][pad] == PadState::DISABLED
+    light_pad pad, PadState::ENABLED
   else
-    light_pad pad, Color::DISABLED
+    light_pad pad, PadState::DISABLED
+  end
+end
+
+define :update_all_lights do
+  for (key, value) in get[:pads_state]
+    light_pad key, value
   end
 end
 
 ## ============= ##
 
-##| daw_mode true
+## Init
+
+if not configured
+  print "configuring"
+  
+  set :configured, true
+  
+  state = pads.reduce({}) do |hash, value|
+    hash[value] = PadState::DISABLED
+    hash
+  end
+  
+  set :pads_state, state
+  
+  daw_mode true
+  update_all_lights
+end
+
+## Play
+
+live_loop :bass do
+  tick
+  use_synth_defaults sustain: 3, release: 2,
+    amp: 0.5
+  use_merged_synth_defaults note: :f3 + (ring 0).look if get[:pads_state][pads[8]] == PadState::ENABLED
+  use_merged_synth_defaults note: :f3 + (ring 0,0,3,-2).stretch(2).look if get[:pads_state][pads[8]] == PadState::DISABLED
+  synth :hollow if get[:pads_state][pads[0]] == PadState::ENABLED
+  synth :growl if get[:pads_state][pads[1]] == PadState::ENABLED
+  sleep 4
+end
+
+slow_kick = "/Users/vladimirbrejcha/Library/Mobile Documents/com~apple~CloudDocs/Projects/SonicPi/Samples/Myloops Free Techno Sample Pack/KIT_05_126BPM_G#MAJ/KIT_05_Kick_With_Bass_126BPM_G#MAJ.wav", 7
+
+with_fx :reverb, room: 0.5 do
+  live_loop :slow_kick do
+    sample slow_kick, amp: 0.5, finish: 0.2, rate: 0.744, pitch: 0.3, cutoff: 120 if get[:pads_state][pads[7]] == PadState::ENABLED
+    sleep 4
+  end
+end
